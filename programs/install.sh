56d89114ec2946256818277a4036b250a771c969
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2017-03-23 12:49:36 +0000

    The terminal-programs will not be tracked by this repo
    Changed it from being a submodule to basically having the install script manage
    how it is cloned and pulled. Would be easier in the future when I want to keep
    updating the program and not having to push and rebase every branch every time

--- programs/install.sh
+++ programs/install.sh
@@ -1,3 +1,29 @@
 #! /bin/bash
 
-cd programs/terminal-programs && make
+program_dir=terminal-programs
+program_url="https://github.com/Irubataru/terminal-programs.git"
+
+full_program_dir="programs/${program_dir}"
+
+echo ''
+echo "Configuring ${program_dir} ..."
+echo ''
+
+if [ ! -d "${full_program_dir}" ]; then
+  echo "No ${program_dir} found, cloning ..."
+  git clone ${program_url} ${full_program_dir}
+	echo ''
+else
+	echo "Do you want to update the ${program_dir} repository?";
+	select updaterepo in "Yes" "No"; do
+		case $updaterepo in
+			Yes ) cd ${full_program_dir} && git pull; break;;
+			No ) break;;
+		esac
+	done
+	echo ''
+fi
+
+echo "Compiling the programs ..."
+cd ${full_program_dir} && make
+echo ''
