71a02f497f79d1d4de82c48131e1a57d4f7761bd
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2020-03-27 12:35:16 +0100

    Added cd alias

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -6,6 +6,9 @@ alias la='ls -a'
 alias ll='ls -l'
 alias lla='ls -al'
 
+# Useful cd aliases
+alias cr='cd $(git rev-parse --show-toplevel)'
+
 # Verbose file operations
 alias rm='rm -v'
 alias mv='mv -v'
