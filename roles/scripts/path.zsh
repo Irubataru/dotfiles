a365569637aef603e70e590dff7ac4bacbc9bea5
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2021-08-15 14:53:55 +0200

    Moved terminal-scripts into dotfiles

--- roles/scripts/path.zsh
+++
@@ -1,8 +0,0 @@
-dir=/home/glesaaen/.dotfiles/roles/scripts/terminal-scripts
-source ${dir}/modules.sh
-
-for module in ${SCRIPT_MODULES[@]}; do
-  export PATH="${PATH}:${dir}/${module}"
-done
-
-export PATH="${PATH}:/home/glesaaen/.usr/bin"
