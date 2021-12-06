adb27c8934f00fe4d6b565255304aeaca231ddee
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2021-12-06 01:14:18 +0100

    More lsp fixing in neovim

--- roles/neovim/config/lua/plugins/null_ls.lua
+++ roles/neovim/config/lua/plugins/null_ls.lua
@@ -2,7 +2,8 @@ local null_ls = require("null-ls")
 
 null_ls.config({
   sources = {
-    null_ls.builtins.formatting.eslint,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua.with({
       extra_args = { "--config-path", vim.fn.expand("~/.config/stylua/stylua.toml") },
     }),
@@ -12,4 +13,4 @@ null_ls.config({
 require("lspconfig")["null-ls"].setup({})
 
 local keymap = vim.api.nvim_set_keymap
-keymap('n', "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<cr>", {noremap = true})
+keymap('n', "<leader>cf", "<cmd>lua vim.lsp.buf.formatting_seq_sync()<cr>", {noremap = true})
