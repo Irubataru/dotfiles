420dfb5043ee1dc17790ea6c479f4381a9d73f73
Aleksandra Rylund Glesaaen <aleksandra@glesaaen.com>
2022-07-26 18:27:25 +0200

    feat(awesome): improve screenshot module

--- roles/awesome/config/modules/screenshot.lua
+++ roles/awesome/config/modules/screenshot.lua
@@ -1,21 +1,28 @@
 local awful = require("awful")
 local config = require("config.config")
 
+local cmd = "maim --hidecursor --quiet --format='png'"
+
 local M = {}
 
 M.printscreen = function()
+  local save_location = config.screenshot.default_location .. "/"
   local date = os.date("%d-%m-%y_%H.%M")
-  local testfile = config.screenshot.default_location .. date .. ".png"
+  local testfile = save_location .. date .. ".png"
+
+  -- If the file exists then try to suffix it
   local prtscr_c = 1
   while awful.util.file_readable(testfile) and (prtscr_c < 100) do
-    testfile = config.screenshot.default_location .. date .. "_" .. prtscr_c .. ".png"
+    testfile = save_location .. date .. "_" .. prtscr_c .. ".png"
     prtscr_c = prtscr_c + 1
   end
-  os.execute("import -window root " .. testfile)
+
+  -- Finally execute the print screen
+  os.execute(cmd .. " \"" .. testfile .. "\"")
 end
 
 M.snip = function()
-  os.execute("maim -s | xclip -selection clipboard -t image/png")
+  os.execute(cmd .. " --select --highlight --color=0.85,0.87,0.91,0.2 | xclip -selection clipboard -t image/png")
 end
 
 return M
--- roles/awesome/config/widgets/audio.lua
+++ roles/awesome/config/widgets/audio.lua
@@ -7,14 +7,14 @@ local M = {}
 
 M.iconbox = wibox.widget.imagebox(icons.full)
 
-M.widget = lain.widget.alsa({
+M.widget = lain.widget.pulse({
     settings = function()
-      if volume_now.status == "off" then
+      if volume_now.muted ~= "no" then
         M.iconbox:set_image(icons.mute)
       else
-        if tonumber(volume_now.level) < 30 then
+        if tonumber(volume_now.left) < 30 then
           M.iconbox:set_image(icons.low)
-        elseif tonumber(volume_now.level) < 60 then
+        elseif tonumber(volume_now.left) < 60 then
           M.iconbox:set_image(icons.medium)
         else
           M.iconbox:set_image(icons.full)
@@ -22,7 +22,7 @@ M.widget = lain.widget.alsa({
       end
 
       local markup = lain.util.markup
-      widget:set_markup(markup.fontfg(theme.font, theme.fg_normal, volume_now.level .. "% "))
+      widget:set_markup(markup.fontfg(theme.font, theme.fg_normal, volume_now.left .. "% "))
     end
 })
 
