706882c5812b42fee8551e3d7a51521b738a35a1
Aleksandra Rylund Glesaaen <aleksandra@glesaaen.com>
2022-10-26 15:04:24 +0200

    fix: updates due to plugin/dependency changes

--- roles/neovim/config/lua/irubataru/lsp/init.lua
+++ roles/neovim/config/lua/irubataru/lsp/init.lua
@@ -54,7 +54,7 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local options = {
   on_attach = on_attach,
