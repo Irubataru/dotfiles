ce2d07edb6ad0322a53584a40af741d2c7a8dcc5
Aleksandra Rylund Glesaaen <aleksandra@glesaaen.com>
2023-10-25 12:02:18 +0200

    feat(nvim): update treesitter config
    Specify the languages I want, so it doesn't install all, and set it to
    be disables for long json files (but this doesn't work very well).

--- roles/neovim/config/lua/irubataru/modules/treesitter.lua
+++ roles/neovim/config/lua/irubataru/modules/treesitter.lua
@@ -9,6 +9,7 @@ local M = {
     "TSDisable",
     "TSModuleInfo",
   },
+  event = "VeryLazy",
 }
 
 M.dependencies = {
@@ -44,22 +45,82 @@ M.config = function()
   local treesitter_config = require("nvim-treesitter.configs")
 
   treesitter_config.setup({
-    ensure_installed = "all",
+    ensure_installed = {
+      "bash",
+      "bibtex",
+      "c",
+      "c_sharp",
+      "clojure",
+      "cmake",
+      "commonlisp",
+      "cpp",
+      "css",
+      "csv",
+      "diff",
+      "dockerfile",
+      "fortran",
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "html",
+      "http",
+      "ini",
+      "javascript",
+      "jq",
+      "json",
+      "json5",
+      "latex",
+      "lua",
+      "luadoc",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "ninja",
+      "python",
+      "r",
+      "regex",
+      "ruby",
+      "rust",
+      "scss",
+      "sql",
+      "ssh_config",
+      "toml",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "vue",
+      "xml",
+      "yaml",
+    },
     ignore_install = { "tex", "latex" }, -- list of parsers to ignore installing
+
     highlight = {
       enable = true,
-      disable = { "tex", "latex" }, -- list of languages that will be disabled
+      disable = function(lang, bufnr)
+        if (lang == "tex" or lang == "latex") then
+          return true
+        end
+
+        -- Disable for large json files
+        -- TODO: This doesn't seem to work?
+        if (lang == "json") then
+          return vim.api.nvim_buf_line_count(bufnr) > 10000
+        end
+
+      end, -- list of languages that will be disabled
 
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
       additional_vim_regex_highlighting = false,
+    },
 
-      indent = {
-        enable = true,
-        disable = {}, -- list of languages not to indent for
-      },
+    indent = {
+      enable = true,
+      disable = {}, -- list of languages not to indent for
     },
   })
 end
