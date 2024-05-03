73d48ef4727e8e6f1edd17962f8abfce6691aa7a
Aleksandra Rylund Glesaaen <aleksandra@glesaaen.com>
2024-05-03 14:54:21 +0200

    chore(nvim): update plugins and install action-hints

---
+++ roles/neovim/config/lua/irubataru/modules/action-hints.lua
@@ -0,0 +1,11 @@
+-- ⚡ A Neovim plugin that displays available actions like 'Go to Definition' and 'Go to Reference(s)'
+-- for the highlighted word, presented in the statusline or inline as virtual text.
+
+-- NOTE: I use this in lualine to display number of references
+
+local M = {
+  "roobert/action-hints.nvim",
+  config = true,
+}
+
+return M
