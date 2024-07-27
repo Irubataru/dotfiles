1b375ac2173bcd1a8b89dac692bba415bdc639ad
Aleksandra Rylund Glesaaen <aleksandra@glesaaen.com>
2024-07-27 08:51:59 +0200

    feat: add hyprland laptop attempt and hyprland config

--- roles/wezterm/config/wezterm.lua
+++ roles/wezterm/config/wezterm.lua
@@ -4,6 +4,9 @@ local config = wezterm.config_builder()
 
 config.check_for_updates = false
 
+config.enable_wayland = false
+
+
 require("appearance").apply(config)
 require("keymaps").apply(config)
 
