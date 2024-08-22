8645a6bec4010ed4b3187d460a80034233a3c9a2
Aleksandra Rylund Glesaaen <aleksandra@glesaaen.com>
2024-08-22 17:57:30 +0200

    fix(nvim,lsp): disable ltex for non-latex files

--- roles/neovim/config/lua/plugins/lang/ltex.lua
+++ roles/neovim/config/lua/plugins/lang/ltex.lua
@@ -2,8 +2,14 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
+      vim.print(opts.servers.ltex)
       opts.servers = vim.tbl_deep_extend("force", opts.servers, {
         ltex = {
+          filetypes = {
+            "bib",
+            "plaintex",
+            "tex",
+          },
           settings = {
             ltex = {
               diagnosticSeverity = "error",
