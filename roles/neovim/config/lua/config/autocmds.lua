a7c75cea4601f88279e7005ff5f19c7c0e913ecb
Aleksandra Rylund Glesaaen <aleksandra@glesaaen.com>
2024-08-22 17:31:26 +0200

    fix(nvim): Fix issue with autocmd and Telescope

--- roles/neovim/config/lua/config/autocmds.lua
+++ roles/neovim/config/lua/config/autocmds.lua
@@ -2,16 +2,17 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- FIX: This doesn't work, runs LSP in telescope prompt
 -- NOTE: New files with empty filetype should be type "text"
--- vim.api.nvim_create_autocmd("BufEnter", {
---   group = vim.api.nvim_create_augroup("empty_filetype", { clear = true }),
---   callback = function()
---     if vim.bo.filetype == "" then
---       vim.bo.filetype = "text"
---     end
---   end,
--- })
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("empty_filetype", { clear = true }),
+  callback = function()
+    vim.schedule(function()
+      if vim.bo.filetype == "" then
+        vim.bo.filetype = "text"
+      end
+    end)
+  end,
+})
 
 -- Turn on autoformat for certain filetypes
 vim.api.nvim_create_autocmd({ "FileType" }, {
