a555deb83978d57a8d34f3a1b5cf7ce68bc81db4
Aleksandra Rylund Glesaaen <aleksandra@glesaaen.com>
2024-10-03 08:56:59 +0200

    feat(nvim,copilot): Keymaps to toggle copilot, fix which-key icon

---
+++ roles/neovim/config/lua/plugins/coding/copilot.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    keys = {
+      {
+        "<leader>at",
+        function()
+          require("copilot.command").toggle()
+        end,
+        desc = "Toggle copilot (buffer)",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>aT",
+        function()
+          local is_disabled = require("copilot.client").is_disabled()
+
+          if is_disabled then
+            require("copilot.command").enable()
+            LazyVim.info("Enabled Copilot", { title = "Copilot" })
+          else
+            require("copilot.command").disable()
+            LazyVim.warn("Disabled Copilot", { title = "Copilot" })
+          end
+        end,
+        desc = "Toggle copilot (global)",
+        mode = { "n", "v" },
+      },
+    },
+  },
+}
