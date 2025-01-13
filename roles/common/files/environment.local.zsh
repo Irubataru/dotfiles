e4375cdc887a32dd87a9982071a40d62cacef615
Aleksandra Glesaaen Ødegård <aleksandra@glesaaen.com>
2025-01-13 13:05:21 +0100

    fix(apps): remove compiler envs and add apps to mimelist

--- roles/common/files/environment.local.zsh
+++ roles/common/files/environment.local.zsh
@@ -1,8 +1,2 @@
 export EDITOR=nvim
 export VIDUAL=nvim
-export CC=clang
-export CXX=clang++
-export GCC=gcc
-export MPI_HOME=/usr
-export XDG_CONFIG_HOME=${HOME}/.config
-export LD_LIBRARY_PATH=/opt/intel/mkl/lib/intel64_lin
