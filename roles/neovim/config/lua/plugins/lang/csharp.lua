c296eb85d77e489e034f6e4af4e9672a5133df9e
Aleksandra Glesaaen Ødegård <aleksandra@glesaaen.com>
2025-02-14 08:00:30 +0100

    fix(nvim): Omnisharp keymap issue + updates
    The default omnisharp keymap depended on Telescope being available, now
    that it isn't, I needed to fix it.

--- roles/neovim/config/lua/plugins/lang/csharp.lua
+++ roles/neovim/config/lua/plugins/lang/csharp.lua
@@ -28,4 +28,34 @@ return {
       end, { desc = "Debug project" })
     end,
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        omnisharp = {
+          handlers = {
+            ["textDocument/definition"] = function(...)
+              return require("omnisharp_extended").handler(...)
+            end,
+          },
+          keys = {
+            {
+              "gd",
+              function()
+                if LazyVim.has("telescope.nvim") then
+                  return require("omnisharp_extended").telescope_lsp_definitions()
+                else
+                  return require("omnisharp_extended").lsp_definitions()
+                end
+              end,
+              desc = "Goto Definition",
+            },
+          },
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
+        },
+      },
+    },
+  },
 }
