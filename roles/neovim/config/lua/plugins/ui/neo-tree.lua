922283fa0765ba32ca6d6dd6233cb3cb4cc3f334
Aleksandra Glesaaen Ødegård <aleksandra@glesaaen.com>
2025-02-17 17:12:00 +0100

    fix(nvim): Do not open windows from neotree in edgy

--- roles/neovim/config/lua/plugins/ui/neo-tree.lua
+++ roles/neovim/config/lua/plugins/ui/neo-tree.lua
@@ -1,6 +1,9 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    init = function() end,
+    opts = {
+      -- NOTE: https://github.com/folke/edgy.nvim/discussions/4
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "edgy" },
+    },
   },
 }
