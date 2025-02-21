4fb7fa9e3abcf295479606ba76a81833875ceb50
Aleksandra Glesaaen Ødegård <aleksandra@glesaaen.com>
2025-02-21 09:51:10 +0100

    fix(bitwarden): Update login scripts and vars

--- roles/bitwarden/alias.zsh
+++ roles/bitwarden/alias.zsh
@@ -1,6 +1,4 @@
 alias bw-login="source _bw-bin-login-script"
 alias bw-unlock="source _bw-bin-unlock-script"
-alias pwdfzf="source _pwdfzf-bin-script"
-alias pwdshow="source _pwdshow-bin-script"
-alias pwdpgp="source _pwdpgp-bin-script"
-alias pwdssh="source _pwdssh-bin-script"
+alias pwd-fzf="source _pwdfzf-bin-script"
+alias pwd-show="source _pwdshow-bin-script"
