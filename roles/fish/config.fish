ffb47e447378ca78e8d7b3eca93da03352c67ab0
Aleksandra Glesaaen Ødegård <aleksandra@glesaaen.com>
2025-06-20 08:58:48 +0200

    fix(fish): support ... and ....

--- roles/fish/config.fish
+++ roles/fish/config.fish
@@ -5,6 +5,10 @@ end
 # Add paths
 fish_add_path ~/.local/bin
 
+# Abbreviations
+abbr -a ... '../..'
+abbr -a .... '../../..'
+
 # Commands to run in interactive sessions
 if status is-interactive
 
