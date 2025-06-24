32af2dc5337ea5487b2326f5b802c5f6aa168889
Aleksandra Glesaaen Ødegård <aleksandra@glesaaen.com>
2025-06-24 15:42:47 +0200

    feat(wezterm): prettier tabs

--- roles/wezterm/config/tabs.lua
+++ roles/wezterm/config/tabs.lua
@@ -15,10 +15,55 @@ local function tab_title(tab_info)
   return tab_info.active_pane.title
 end
 
+local function split_string(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for part in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    table.insert(t, part)
+  end
+  return t
+end
+
+-- Function to simplify paths by showing only the current directory
+local function simplify_path(path_str)
+  -- If it doesn't look like a path, return as-is
+  if not string.match(path_str, "/") then
+    return path_str
+  end
+
+  -- Extract just the final directory name
+  local final_dir = string.match(path_str, "/([^/]+)/?$")
+  if final_dir then
+    return final_dir
+  end
+
+  -- Fallback: if no final directory found, return the original
+  return path_str
+end
+
+local wezterm = require("wezterm")
+
+-- Icon mappings for common programs
+local program_icons = {
+  vim = wezterm.nerdfonts.custom_neovim,
+  nvim = wezterm.nerdfonts.custom_neovim,
+  git = wezterm.nerdfonts.dev_git,
+  node = wezterm.nerdfonts.dev_nodejs_small,
+  npm = wezterm.nerdfonts.dev_npm,
+  python = wezterm.nerdfonts.dev_python,
+  lua = wezterm.nerdfonts.dev_lua,
+  fish = wezterm.nerdfonts.fa_fish,
+  docker = wezterm.nerdfonts.dev_docker,
+  ["docker-compose"] = wezterm.nerdfonts.dev_docker .. "  compose",
+  lazygit = wezterm.nerdfonts.dev_git .. "  lazygit",
+  lg = wezterm.nerdfonts.dev_git .. "  lazygit",
+  ncspot = wezterm.nerdfonts.fa_spotify .. "  ncspot",
+}
+
 ---Function that controls the titles of the wezterm tabs
 M.format_tab_title = function()
-  local wezterm = require("wezterm")
-
   wezterm.on(
     "format-tab-title",
     ---Function that controls the titles of the wezterm tabs
@@ -31,7 +76,47 @@ M.format_tab_title = function()
     ---@return unknown
     function(tab, tabs, panes, config, hover, max_width)
       local title = tab_title(tab)
-      return title
+      local components = split_string(title, "%s")
+
+      if #components == 0 then
+        return title
+      end
+
+      local program = components[1]
+
+      -- Check if the "program" is actually a path (starts with ~ or /)
+      if string.match(program, "^[~/]") then
+        local folder_icon = wezterm.nerdfonts.md_folder
+        return folder_icon .. "  " .. simplify_path(program)
+      end
+
+      -- Assume pwd is always the last part of the string, remove it
+      if #components > 1 then
+        table.remove(components, #components)
+      end
+
+      -- If we have an icon for this program, use it
+      local icon = program_icons[program]
+      if icon then
+        if #components > 1 then
+          -- Join remaining args (excluding the program name)
+          local args = {}
+          for i = 2, #components do
+            table.insert(args, components[i])
+          end
+          return icon .. "  " .. table.concat(args, " ")
+        else
+          return icon
+        end
+      end
+
+      -- For programs without icons, show program + args with simplified paths
+      local result_components = { program }
+      for i = 2, #components do
+        local component = simplify_path(components[i])
+        table.insert(result_components, component)
+      end
+      return table.concat(result_components, " ")
     end
   )
 end
