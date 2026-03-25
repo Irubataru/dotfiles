c9de881c694e4919498439a375c222bbe07dd42d
Aleksandra Glesaaen Ødegård <aleksandra@glesaaen.com>
2026-03-25 08:34:22 +0100

    fix(zellij): update to v0.44
    In v0.44 there is a build-in layout manager, meaning that I don't really
    need the welcome screen anymore. Temporarly disabled by commenting out.

--- roles/zellij/fish/zellij.fish
+++ roles/zellij/fish/zellij.fish
@@ -1,17 +1,17 @@
 if status is-interactive
 
-  set ZELLIJ_AUTO_EXIT true
-  if not set -q ZELLIJ
-    if test "$ZELLIJ_AUTO_ATTACH" = "true"
-        zellij attach -c
-    else
-        zellij -l welcome
-    end
+  # set ZELLIJ_AUTO_EXIT true
+  # if not set -q ZELLIJ
+  #   if test "$ZELLIJ_AUTO_ATTACH" = "true"
+  #       zellij attach -c
+  #   else
+  #       zellij -l welcome
+  #   end
+  #
+  #   if test "$ZELLIJ_AUTO_EXIT" = "true"
+  #       kill $fish_pid
+  #   end
+  # end
 
-    if test "$ZELLIJ_AUTO_EXIT" = "true"
-        kill $fish_pid
-    end
-  end
-
-  # eval (zellij setup --generate-auto-start fish | string collect)
+  eval (zellij setup --generate-auto-start fish | string collect)
 end
