1d31e1b2e782907edb8e44871a4f6c124f116fd6
Aleksandra Glesaaen Ødegård <aleksandra@glesaaen.com>
2026-07-21 14:38:04 +0200

    fix(nvim,csharp): re-added csharp plugin

--- roles/neovim/config/lua/plugins/lang/csharp.lua
+++ roles/neovim/config/lua/plugins/lang/csharp.lua
@@ -42,6 +42,33 @@ return {
     opts = { ensure_installed = { "csharpier", "netcoredbg", "fantomas" } },
   },
   {
+    "iabdelkareem/csharp.nvim",
+    dependencies = {
+      "Tastyep/structlog.nvim",
+    },
+    ft = { "cs" },
+    opts = {
+      lsp = {
+        omnisharp = {
+          enable = false,
+        },
+      },
+    },
+    init = function()
+      vim.keymap.set("n", "<leader>xa", function()
+        require("csharp").fix_all()
+      end, { desc = "Fix all (C#)" })
+
+      vim.keymap.set("n", "<leader>cv", function()
+        require("csharp").view_user_secrets()
+      end, { desc = "View user secrets (C#)" })
+
+      vim.keymap.set("n", "<leader>dd", function()
+        require("csharp").debug_project()
+      end, { desc = "Debug project" })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
